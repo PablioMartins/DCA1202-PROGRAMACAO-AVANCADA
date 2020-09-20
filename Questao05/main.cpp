94bede6dbdb609ed55218d062f6b50494a92077c
Pablio Martins <pablo.martins.anjos@hotmail.com>
2020-09-20 01:05:53 -0300

    Q5 e Q6

---
+++ Questao05/main.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void){
+  float vet[5] = {1.1,2.2,3.3,4.4,5.5};
+  // atribucao dos valores do array "vet"
+
+  float *f;
+  int i;
+  f = vet;
+  // ponteiro "f" apontado para "vet", valor guardado em "f" e o endereco de "vet[0]"
+
+  printf("contador/valor/valor/endereco/endereco");
+  for(i = 0 ; i <= 4 ; i++){
+  printf("\ni = %d | ",i);
+  // escreve na tela o valor do contador
+
+  printf("vet[%d] = %.1f | ",i, vet[i]);
+  // escreve na tela o valor contido em "vet" na posicao "i".
+
+  printf("*(f + %d) = %.1f | ",i, *(f+i));
+  // escreve na tela o conteudo associado ao ponteiro "f" na posicao "i", apartir do ponteiro "f".
+
+  printf("&vet[%d] = %X | ",i, &vet[i]);
+  // escreve na tela o endereco do array "vet" na posicao "i", apartir do proprio array.
+
+  printf("(f + %d) = %X",i, f+i);
+  // escreve na tela o endereco do array "vet" na posicao "i", apartir do ponteiro "f"
+
+  }
+  printf("\n");
+}
---
+++ Questao06/main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+    int pulo[6] = {1, 2, 3, 4, 5, 6};
+    printf("%d\n", *(pulo + 2)); //faz referencia ao valor na posicao "3" do array. <-- Resposta
+    printf("%d\n", *(pulo + 4)); //faz referencia ao valor na posicao "5" do array.
+    printf("%p\n", pulo + 2); //faz referencia ao endereco de memoria da posicao "3"
+    printf("%p\n", pulo + 4); //faz referencia ao endereco de memoria da posicao "5".
+    return 0;
+}
